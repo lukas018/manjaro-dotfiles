aac6dec82e300d5a58ffeefdf20255197df1c0f9
Lukas Lundmark <lukas018.lundmark@gmail.com>
2021-06-19 14:47:26 +0200

    Added polybar config

---
+++ .config/polybar/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pkill -u $UID -x polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+export MONITOR=HDMI-2
+polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
+echo 'Polybar launched...'
