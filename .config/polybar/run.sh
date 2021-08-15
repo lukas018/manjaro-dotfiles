6b1f4f0f7776ce5e3aeff34d13250670880a3271
lukas018 <lukas018.lundmark@gmail.com>
2021-08-15 16:41:15 +0200

    Smaller adjustments

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -3,10 +3,9 @@
 pkill -u $UID -x polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-export MONITOR=HDMI-2
-polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
-echo 'Polybar launched monitor1'
-
+# export MONITOR=HDMI-2
+# polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
+# echo 'Polybar launched monitor1'
 
 export MONITOR=eDP-1
 polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
