0f6866f652fb612279ce99203a62070c06779abd
Lukas Lundmark <lukas018.lundmark@gmail.com>
2021-11-04 15:15:36 +0100

    Modified polybar

--- .config/polybar/run.sh
+++ .config/polybar/run.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-pkill -u $UID -x polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# export MONITOR=HDMI-2
-# polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
-# echo 'Polybar launched monitor1'
+pkill polybar
+while pgrep polybar >/dev/null; do sleep 1; done
 
 export MONITOR=eDP-1
 polybar main >$XDG_DATA_HOME/polybar.log 2>&1 &
